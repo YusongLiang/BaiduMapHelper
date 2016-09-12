330fe506d9082fa392a96f8cf4b1aa55fa77c70e
Felix Liang <lys74123@163.com>
2016-09-12 20:45:01 +0800

    更新了部分文档

--- baidumaphelper/src/main/java/com/github/yusongliang/library/util/WaveAnimation.java
+++ baidumaphelper/src/main/java/com/github/yusongliang/library/util/WaveAnimation.java
@@ -1,7 +1,42 @@
 package com.github.yusongliang.library.util;
 
+import com.baidu.mapapi.map.CircleOptions;
+import com.baidu.mapapi.model.LatLng;
+
 /**
  * 波浪动画
  */
 public class WaveAnimation {
+    private LatLng mCenter;
+    private int mFillColor;
+    private UpdateCircleThread mThread;
+
+    public WaveAnimation(LatLng center, int fillColor) {
+        mCenter = center;
+        mFillColor = fillColor;
+    }
+
+    public WaveAnimation(float cLat, float cLng, int fillColor) {
+        mCenter = new LatLng(cLat, cLng);
+        mFillColor = fillColor;
+    }
+
+    /**
+     * 画圆
+     *
+     * @param radius 圆半径
+     */
+    private void drawCircle(int radius) {
+        CircleOptions circleOptions = new CircleOptions()
+                .center(mCenter)
+                .fillColor(mFillColor)
+                .radius(radius);
+    }
+
+    /**
+     * 更新圆圈半径
+     */
+    private class UpdateCircleThread extends Thread {
+
+    }
 }
